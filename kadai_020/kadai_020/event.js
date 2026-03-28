ad08fc4fa08a28e4f33664637fa82a577d7f971d
Trinh-Thi-Trinh <trinhtrinh519@gmail.com>
2026-03-28 18:08:20 +0900

    Delete kadai_020/kadai_020/event.js

--- kadai_020/kadai_020/event.js
+++
@@ -1,8 +0,0 @@
-
-const addText = document.getElementById ("text");
-const addBtn = document.getElementById ("btn");
-addBtn.addEventListener ('click', () => {
-  addtext.textContent ="ボタンをクリックされました";
-  console.log("ボタンをクリックされました")
-})
-
